b7894bfbbeb63d13ee7bcd74b4a5e4bc3c6416da
Chan <Lenovo@host.docker.internal>
2020-07-29 16:18:09 +0630

    29..07.2020

---
+++ ThymeLeafErrorPage/src/main/java/com/chan/spring/ThymeLeafErrorPageApplication.java
@@ -0,0 +1,13 @@
+package com.chan.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafErrorPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafErrorPageApplication.class, args);
+	}
+
+}
