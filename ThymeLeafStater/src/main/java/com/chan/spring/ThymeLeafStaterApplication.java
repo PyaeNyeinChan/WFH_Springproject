882a7da74a10b1911f38e7cbea77d2b6349df55d
Chan <Lenovo@host.docker.internal>
2020-07-29 16:21:10 +0630

    29.07.2020

---
+++ ThymeLeafStater/src/main/java/com/chan/spring/ThymeLeafStaterApplication.java
@@ -0,0 +1,13 @@
+package com.chan.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafStaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafStaterApplication.class, args);
+	}
+
+}
---
+++ ThymeLeafStater/src/main/java/com/chan/spring/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.chan.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("message","Thank's for everything.");
+		return "index";
+	}
+}
