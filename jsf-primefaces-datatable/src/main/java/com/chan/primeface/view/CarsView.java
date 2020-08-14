d89f2717945d59c9ae8b5f06474dc7bfab576352
Chan <Lenovo@host.docker.internal>
2020-08-14 16:59:44 +0630

    14.08.2020

--- jsf-primefaces-datatable/src/main/java/com/chan/primeface/view/CarsView.java
+++ jsf-primefaces-datatable/src/main/java/com/chan/primeface/view/CarsView.java
@@ -3,12 +3,16 @@ package com.chan.primeface.view;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import org.primefaces.event.SelectEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.chan.primeface.model.Car;
@@ -22,13 +26,23 @@ public class CarsView implements Serializable {
 
 	private List<Car> cars;
 
+	private Car selectedCar;
+
 	@Autowired
 	private CarRepository carRepository;
 
 	@PostConstruct
 	public void init() {
 		cars = carRepository.findAll();
-		System.out.println("cars:" + cars);
+		// System.out.println("cars:" + cars);
+	}
+
+	public Car getSelectedCar() {
+		return selectedCar;
+	}
+
+	public void setSelectedCar(Car selectedCar) {
+		this.selectedCar = selectedCar;
 	}
 
 	public List<Car> getCars() {
@@ -37,6 +51,14 @@ public class CarsView implements Serializable {
 
 	public void setCars(List<Car> cars) {
 		this.cars = cars;
+	}
+
+	public void delete(Long id) {
+		carRepository.deleteById(id);
+	}
+	
+	public void selectID() {
+		System.out.println(selectedCar.getId());
 	}
 
 }
